dc6bc11ccf5d257b8773626fa67c18cb76e84020
Your Name <your.email@example.com>
2026-04-23 14:06:46 +0530

    add ingress route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const API= axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
